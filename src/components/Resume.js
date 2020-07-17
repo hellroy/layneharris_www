0103bbb97cbc35cc7855660bfb2f4513f8d55058
Layne Harris <helsmack@gmail.com>
2020-07-17 16:06:21 -0400

    rearranging some sections

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,33 +4,6 @@ export default  class Resume extends Component {
     let resumeData = this.props.resumeData;
     return (
       <section id="resume">
-
-         <div className="row education">
-
-            <div className="three columns header-col">
-               <h1><span>Education</span></h1>
-            </div>
-
-            <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
-                  return(
-                    <div className="row item">
-                       <div className="twelve columns">
-                          <h3>{item.UniversityName}</h3>
-                          <p className="info">
-                          {item.specialization}
-                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
-                          <p>
-                          {item.Achievements}
-                          </p>
-                       </div>
-                    </div>
-                  )
-                })
-              }
-            </div>
-         </div>
         <div className="row work">
             <div className="three columns header-col">
                <h1><span>Work</span></h1>
@@ -58,9 +31,33 @@ export default  class Resume extends Component {
               }
             </div> 
          </div>
+        <div className="row education">
 
+<div className="three columns header-col">
+   <h1><span>Education</span></h1>
+</div>
 
-         <div className="row skill">
+<div className="nine columns main-col">
+  {
+    resumeData.education && resumeData.education.map((item)=>{
+      return(
+        <div className="row item">
+           <div className="twelve columns">
+              <h3>{item.UniversityName}</h3>
+              <p className="info">
+              {item.specialization}
+              <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+              <p>
+              {item.Achievements}
+              </p>
+           </div>
+        </div>
+      )
+    })
+  }
+</div>
+</div>
+        <div className="row skill">
 
             <div className="three columns header-col">
                <h1><span>Skills</span></h1>
@@ -93,7 +90,6 @@ export default  class Resume extends Component {
    			</div>
 
          </div>
-
       </section>
     );
   }

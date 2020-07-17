903c1db8089926adb05f041d6b5b3e5b3a0548e5
Layne Harris <helsmack@gmail.com>
2020-07-17 16:17:25 -0400

    updated main graphic

--- src/resumeData.js
+++ src/resumeData.js
@@ -4,7 +4,7 @@ let resumeData = {
     "role": "Frontend Developer and Data Scientist",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "Award-winning Creative Technologist",
     "socialLinks":[
         {
           "name":"linkedin",

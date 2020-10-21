99c536fff432e517bafa58aece7ffde0ecab1301
Layne Harris <helsmack@gmail.com>
2020-10-21 12:16:44 -0400

    Update resumeData.js
    removed stars

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,7 +1,7 @@
 let resumeData = {
     "imagebaseurl":"https://layneharris.com/",
     "name": "Layne Harris",
-    "role": "** THIS SITE IS UNDER CONSTRUCTION **",
+    "role": "THIS SITE IS UNDER CONSTRUCTION",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
     "roleDescription": "",
@@ -22,7 +22,7 @@ let resumeData = {
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"** THIS SITE IS UNDER CONSTRUCTION **",
+    "aboutme":"THIS SITE IS UNDER CONSTRUCTION",
     "address":"Brooklyn, NY",
     "website":"https://www.layneharris.com",
     "education":[

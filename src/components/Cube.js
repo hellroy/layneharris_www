6277631628fd7c2f35f380cadf53c17c8202edf8
Layne Harris <helsmack@gmail.com>
2021-03-29 21:16:14 -0400

    Small fix to cube
    cleaned up cube file to get around compiler warnings.

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -4,8 +4,6 @@ import React, {
 
 import * as THREE from "three";
 
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
 export default class About extends Component {
     render() {
 
@@ -34,74 +32,9 @@ export default class About extends Component {
             renderer.render(scene, camera);
         };
 
-
-        // const loader = new GLTFLoader();
-
-        // let mixer;
-        // let model;
-
-        // //let modelReady = false;
-
-        // // Load a glTF resource
-        // loader.load(
-        //     // resource URL
-        //     'models/layne.glb',
-        //     // called when the resource is loaded
-        //     (gltf) => {
-
-        //         mixer = new THREE.AnimationMixer(gltf.scene);
-
-        //         //scene.add(gltf.scene);
-
-        //         gltf.animations; // Array<THREE.AnimationClip>
-        //         gltf.scene; // THREE.Group
-        //         gltf.scenes; // Array<THREE.Group>
-        //         gltf.cameras; // Array<THREE.Camera>
-        //         gltf.asset; // Object
-
-        //         var SManimations = gltf.animations;
-
-        //         if (SManimations && SManimations.length) {
-
-        //             for (var i = 0; i < SManimations.length; i++) {
-        //                 console.log(SManimations[i].name)
-        //                 var SManimation = SManimations[i];
-        //                 var SMaction = mixer.clipAction(SManimation);
-        //                 SMaction.play();
-        //             }
-        //         }
-
-        //         gltf.scene.position.x = position.x;
-        //         gltf.scene.position.y = position.y;
-        //         gltf.scene.position.z = position.z;
-
-        //         //gltf.scene.rotation.y = rotation;
-
-        //         //gltf.scene.scale.set(1, 1, 1) // scale here
-        //         model = gltf.scene;
-        //         model.castShadow = true;
-        //         model.receiveShadow = true;
-        //         //worldOctree.fromGraphNode( model );
-
-        //         this.modelReady = true;
-
-        //         scene.add(model);
-
-        //     },
-        //     // called while loading is progressing
-        //     function (xhr) {
-        //         console.log((xhr.loaded / xhr.total * 100) + '% funhouse loaded');
-        //     },
-        //     // called when loading has errors
-        //     function (error) {
-        //         console.log('A Funhouse error happened');
-        //     },
-
-        // );
-
         animate();
 
-        return ('');
+        return (<React.Fragment></React.Fragment>);
 
     }
 }

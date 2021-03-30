0bb17986bc609dd470b5be7a5cda95c808372bbe
Layne Harris <helsmack@gmail.com>
2021-03-30 09:46:22 -0400

    Cosmetic changes
    Changed font, added post-processing effect

--- src/components/Layne.js
+++ src/components/Layne.js
@@ -8,96 +8,103 @@ import {
     GLTFLoader
 } from "three/examples/jsm/loaders/GLTFLoader";
 
+
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js';
+import { DotScreenShader } from 'three/examples/jsm/shaders/DotScreenShader.js';
+    
+
+
 export default class About extends Component {
+
     render() {
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-        const renderer = new THREE.WebGLRenderer( {antialias: true});
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-
-        camera.position.z = 5;
-
-        const loader = new GLTFLoader();
-
-        const light = new THREE.AmbientLight(0xFF00FF); // soft white light
-        scene.add(light);
-
-        //let mixer;
-        let model;
-
         var modelReady = false;
+        let model;
+        let composer;
 
-        // Load a glTF resource
-        loader.load(
-            // resource URL
-            './models/layne.glb',
-            // called when the resource is loaded
-            (gltf) => {
+            
+                //let resumeData = this.props.resumeData;
 
-                //mixer = new THREE.AnimationMixer(gltf.scene);
+                const scene = new THREE.Scene();
+                const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+                const renderer = new THREE.WebGLRenderer({
+                    antialias: true
+                });
+                renderer.setSize(window.innerWidth, window.innerHeight);
+                document.body.appendChild(renderer.domElement);
 
-                //scene.add(gltf.scene);
+                camera.position.z = 5;
 
-                // gltf.animations; // Array<THREE.AnimationClip>
-                // gltf.scene; // THREE.Group
-                // gltf.scenes; // Array<THREE.Group>
-                // gltf.cameras; // Array<THREE.Camera>
-                // gltf.asset; // Object
+                const loader = new GLTFLoader();
 
-                // var SManimations = gltf.animations;
+                const light = new THREE.AmbientLight(0xFF00FF); // soft white light
+                scene.add(light);
 
-                // if (SManimations && SManimations.length) {
+                //let mixer;
+                
 
-                //     for (var i = 0; i < SManimations.length; i++) {
-                //         console.log(SManimations[i].name)
-                //         var SManimation = SManimations[i];
-                //         var SMaction = mixer.clipAction(SManimation);
-                //         SMaction.play();
-                //     }
-                // }
+                // postprocessing
+                composer = new EffectComposer(renderer);
+                composer.addPass(new RenderPass(scene, camera));
 
-                //gltf.scene.rotation.y = rotation;
+                const effect1 = new ShaderPass(DotScreenShader);
+                effect1.uniforms['scale'].value = 100;
+                composer.addPass(effect1);
 
-                gltf.scene.scale.set(0.05, 0.05, 0.05) // scale here
-                model = gltf.scene;
-                model.castShadow = true;
-                model.receiveShadow = true;
-                //worldOctree.fromGraphNode( model );
+                const effect2 = new ShaderPass(RGBShiftShader);
+                effect2.uniforms['amount'].value = 0.15;
+                composer.addPass(effect2);
 
-                scene.add(model);
+                //
 
-                modelReady = true
+                // Load a glTF resource
+                loader.load(
+                    // resource URL
+                    './models/layne.glb',
+                    // called when the resource is loaded
+                    (gltf) => {
 
-            },
-            // called while loading is progressing
-            function (xhr) {
-                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-            },
-            // called when loading has errors
-            function (error) {
-                console.log('A error happened');
-            },
+                        gltf.scene.scale.set(0.05, 0.05, 0.05) // scale here
+                        model = gltf.scene;
+                        model.castShadow = true;
+                        model.receiveShadow = true;
 
-        );
+                        scene.add(model);
 
-        const animate = function () {
-            requestAnimationFrame(animate);
+                        modelReady = true
 
-            if (modelReady) {
+                    },
+                    // called while loading is progressing
+                    function (xhr) {
+                        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+                    },
+                    // called when loading has errors
+                    function (error) {
+                        console.log('A error happened');
+                    },
 
-                //model.rotation.x += 0.01;
-                model.rotation.y += 0.01;
-            }
+                );
 
-            renderer.render(scene, camera);
-        };
+            const animate = function () {
+                requestAnimationFrame(animate);
 
-        animate();
+                if (modelReady) {
+                    //model.rotation.x += 0.01;
+                    model.rotation.y += 0.01;
+                }
 
-        return (<React.Fragment></React.Fragment>);
+                composer.render();
+               //renderer.render(scene, camera);
+            };
+
+            animate();
+
+        return (<React.Fragment>
+            <section id="about"></section>
+        </React.Fragment>);
 
     }
 }
